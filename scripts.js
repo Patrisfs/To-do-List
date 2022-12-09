d32ed90a17db1c7d4dab9be6c7702af6c9d425d9
patrisfs <patrickfreitas6020@gmail.com>
2022-12-09 10:12:43 -0300

    finalizado

--- scripts.js
+++ scripts.js
@@ -5,7 +5,7 @@ function addTask(){
     if(taskTitle){
         //clonar template
         const template = document.querySelector(".template");
-        const newTask = template.cloneNode(true);
+        const newTask = template.cloneNode(true); //cria um clone do Node
 
         //add títitulo
         newTask.querySelector(".task-title").textContent = taskTitle;
@@ -18,22 +18,41 @@ function addTask(){
         const list = document.querySelector("#tasks-list");
         list.appendChild(newTask);
 
-        //limpar texto da tarefa
+        //adcionar evento de remover
+        const removeBtn = newTask.querySelector("#remove-btn").addEventListener("click", function(e){
+            removeTask(this);     
+        });
+
+        //adicionar evento de completar tarefa
+        const doneBtn =newTask.querySelector("#done-btn").addEventListener("click", function(e){
+            doneTask(this);
+        });
+
+        //limpar texto da tarefa Pai 
         document.querySelector("#task-title").value ="";
     }
 }
 
+//criando função de remover tarefa
+function removeTask(task){
+    task.parentNode.remove(true);
+    console.log(task);
+}
+
+//criando a função de completar tarefa
+function doneTask(task){
+    const taskComplete = task.parentNode;
+    taskComplete.classList.toggle("done");
 
 
-
-
+}
 
 //evento ao cliclar no botão de add tarefa
 var addBtn = document.querySelector("#add-btn");
 
 addBtn.addEventListener("click", function(e){
-    e.preventDefault();
-    addTask();
+    e.preventDefault(); //
+    addTask(); //função que cria tarefas novas
 
 });
 
