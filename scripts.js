aeba46231f35ab15e9425cd72477b24d5f838979
patrisfs <patrickfreitas6020@gmail.com>
2022-12-09 10:14:07 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,7 +1,6 @@
 //evento de add tarefa
 function addTask(){
     const taskTitle = document.querySelector("#task-title").value;
-
     if(taskTitle){
         //clonar template
         const template = document.querySelector(".template");
@@ -43,18 +42,12 @@ function removeTask(task){
 function doneTask(task){
     const taskComplete = task.parentNode;
     taskComplete.classList.toggle("done");
-
-
 }
 
 //evento ao cliclar no botão de add tarefa
 var addBtn = document.querySelector("#add-btn");
-
 addBtn.addEventListener("click", function(e){
     e.preventDefault(); //
     addTask(); //função que cria tarefas novas
 
 });
-
-//evento de remover tarefa
-//evento de completar tarefa
